2a29f4cd9a71027381e30b4a2368c403ffb50951
Amin <amin.sla2015@gmail.com>
2020-10-15 21:05:54 +0200

    pokedex finis

--- script.js
+++ script.js
@@ -26,31 +26,35 @@ function emptyList () {
 function createItem (pokemon) {
     
     const item = document.createElement("li");
-     const pics = document.createElement("img");
+     
+    const pics = document.createElement("img");
+
     
 
     fetch(pokemon.url).then(transformToJson).then((data) => {
         
+        pokemon = {};
         /*pokemon['name'] = data.name;
         pokemon['id'] = data.id;
         pokemon['weight'] = data.weight;
         pokemon['height'] = data.height;
-        pokemon['types'] = data.type.type;
+        pokemon['types'] = data.types;
         pokemon['sprites'] = data.sprites;*/
-
-
-
-        
-        item.textContent = data.name +"  "+data.id; 
-        list.appendChild(item);// li dans ul
-        item.appendChild(pics);
+        item.textContent = data.name;
+        list.appendChild(item); //li dans ul
+        item.appendChild(pics); //pics dans li
         pics.src = data.sprites.front_default;
-        item.addEventListener("mouseenter", (e) => {
-            showDescription(data);
-            item.appendChild(description);
+      
 
+        item.addEventListener("mouseover", (e) => {
+            showDescription(data);            
+            item.appendChild(description);   
+        })
+    
+        item.addEventListener("mouseleave", (e) => {
+            hideDescription("show");
+            item.removeChild(description);
         });
-        
 
     });
     
@@ -62,29 +66,42 @@ function createItem (pokemon) {
 function fillList (json) {
     emptyList();
     json.results.forEach(createItem);
-
+    
 }
 
 /**
  * Fill and display the description
  */
 function showDescription (data) {
-
     description.classList.add("show");
-
+    let types = description.querySelectorAll(".types");
     const fields = description.querySelectorAll("dd");
-    fields.forEach((dd) => { 
-        dd.textContent = data[dd.classList[0]]; // convertie la data brut en un array pour chaque dd.
+
+    fields.forEach((dd) => {
+    dd.textContent = data[dd.classList[0]];
     });
+
+    
+    types.forEach((types) => {
+        if (data.types.length < 2) {
+        types.textContent = data.types[0].type.name; 
+    } else {
+        types.textContent = data.types[0].type.name + " " + data.types[1].type.name; 
+    }
+    
+}); 
+
 }
 
-/**
+
+/*
  * Hide the description
  */
 function hideDescription () {
     description.classList.remove("show");
+
 }
+
 
 // Fetch the API end-point and fill the list
 fetch(api).then(transformToJson).then(fillList);
-
